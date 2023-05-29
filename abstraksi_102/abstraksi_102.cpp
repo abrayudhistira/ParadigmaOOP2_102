16ddf1420adec77291302689690d17f91c80d413
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2023-05-29 19:38:45 +0700

    class abstrak

---
+++ abstraksi_102/abstraksi_102.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class AbstraksiKlas {			//class abstrak
+private: string x, y;
+public:
+	//method untuk mengisi nilai
+	//private number
+	void setXY(string a, string b) {
+		x = a;
+		y = b;
+	}
+	//menampilkan nilai
+	void display() {
+		cout << "x = " << x << endl;
+		cout << "y = " << y << endl;
+	}
+};
+
+int main() {
+	AbstraksiKlas ak;
+	ak.setXY("Yogyakarta", "Kampus");
+		ak.display();
+
+		return 0;
+}
