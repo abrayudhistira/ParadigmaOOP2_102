2946b6e1d50760a134da5817be26e23425234871
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2023-05-29 13:40:46 +0700

    class derivedClass yang diturunkan secara virtual dan publik dari class baseClass

--- noOverriding_102/noOverriding_102.cpp
+++ noOverriding_102/noOverriding_102.cpp
@@ -7,7 +7,7 @@ public:
 		cout << "Halo saya Function dari base class" << endl;
 	}
 };
-class derivedClass : public baseClass {
+class derivedClass : public baseClass {		//class derivedClass yang diturunkan secara virtual dan publik dari class baseClass
 public:
 	void perkenalan() {
 		cout << "halo saya Function dari derived class" << endl;
