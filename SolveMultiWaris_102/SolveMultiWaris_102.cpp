7e6513295917e7185f08acf6c14536d067312b5d
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2023-05-29 11:26:26 +0700

    class orang

---
+++ SolveMultiWaris_102/SolveMultiWaris_102.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+class orang {										//class orang
+public:
+	int umur;
+
+	orang(int pUmur) :
+		umur(pUmur)
+	{
+		cout << "Orang dibuat dengan umur" << endl;
+	}
+};
+
+class pekerja : virtual public orang {
+public:
+	pekerja(int pUmur) :
+		orang(pUmur)
+	{
+		cout << "Pekerja dibuat\n" << endl;
+	}
+};
+
+class pelajar : virtual public orang {
+public:
+	pelajar(int pUmur) :
+		orang(pUmur)
+	{
+		cout << "Pelajar dibuar\n" << endl;
+	}
+};
+
+class budi: public pekerja, public pelajar {
+public:
+
+	budi(int pUmur) :
+		pekerja(pUmur),
+		pelajar(pUmur),
+		orang(pUmur)  //tidak bisa dipakai tanpa virtual
+	{
+		cout << "Budi dibuat\n" << endl;   
+	}
+};
+
+int main() {
+	budi a(12);
+
+	return 0;
+}
